fa49bc0eb0eac081fd6ad35621dca857a11c2446
littlecloud1 <cloudtang030@gmail.com>
2018-02-01 13:02:11 -0800

    section

--- parallel-mergesort.cc
+++ parallel-mergesort.cc
@@ -50,9 +50,14 @@ void Pmerge(keytype* T, int p1, int r1, int p2, int r2, keytype* A, int p3)
 		int q2 = BinarySearch(T[q1], T, p2, r2);
 		int q3 = p3 + (q1 - p1) + (q2 - p2);
 		A[q3] = T[q1];
-		#pragma omp task
-		Pmerge(T, p1, q1 - 1, p2, q2 - 1, A, p3);
-		Pmerge(T, q1 + 1, r1, q2, r2, A, q3 + 1);
+		#pragma omp parallel sections
+		{
+#pragma omp section
+			Pmerge(T, p1, q1 - 1, p2, q2 - 1, A, p3);
+#pragma omp section
+			Pmerge(T, q1 + 1, r1, q2, r2, A, q3 + 1); 
+		
+		}
 	}
 
 
@@ -69,10 +74,13 @@ void Pmergesort(keytype* A, int p, int r, keytype* B, int s)
 		keytype* T = newKeys(n);
 		int q = (p + r) / 2;
 		int qt = q - p + 1;
-		#pragma omp task
-		Pmergesort(A, p, q, T, 1);
-		Pmergesort(A, q + 1, r, T, qt + 1);
-		#pragma omp taskwait
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			Pmergesort(A, p, q, T, 1);
+			#pragma omp section
+			Pmergesort(A, q + 1, r, T, qt + 1); 
+		}
 		Pmerge(T, 1, qt, qt + 1, n, B, s);
 	}
 
